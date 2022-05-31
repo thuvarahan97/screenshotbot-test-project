2b52ce96b28f0d2a24e19c97ba1b584380e882f8
Thuvarahan <thuvarahan97@gmail.com>
2022-05-31 19:45:23 +0530

    fix test for screenshotbot

--- test/index.test.js
+++ test/index.test.js
@@ -3,41 +3,38 @@ const playwright = require('playwright')
 describe('Main & Finish Page', () => {
     let browser;
     let page;
-
-    before(async () => {
+  
+    it('should render in Chrome', async () => {
       browser = await playwright.chromium.launch();
       page = await browser.newPage();
-    });
-  
-    it('should render', async () => {
       await page.goto('http://localhost:8080/');
-      await page.screenshot({ path: 'test/screenshots/main_page.png', fullPage: true });
+      await page.screenshot({ path: 'test/screenshots/main_page--chrome.png', fullPage: true });
       await page.locator('#submitBtn').click();
       setTimeout(function () {
       }, 1000);
-      await page.screenshot({ path: 'test/screenshots/finish_page.png', fullPage: true });
+      await page.screenshot({ path: 'test/screenshots/finish_page--chrome.png', fullPage: true });
     });
 
     after(async () => {
       await browser.close();
     });
   
-    it('should render in firefox', async () => {
+    it('should render in Firefox', async () => {
       browser = await playwright.firefox.launch();
       page = await browser.newPage();
       await page.goto('http://localhost:8080/');
-      await page.screenshot({ path: 'test/screenshots/main_page_firefox.png', fullPage: true });
+      await page.screenshot({ path: 'test/screenshots/main_page--firefox.png', fullPage: true });
       await page.locator('#submitBtn').click();
       setTimeout(function () {
       }, 1000);
-      await page.screenshot({ path: 'test/screenshots/finish_page_firefox.png', fullPage: true });
+      await page.screenshot({ path: 'test/screenshots/finish_page--firefox.png', fullPage: true });
     });
 
     after(async () => {
       await browser.close();
     });
   
-    it('should render in Nexus5', async () => {
+    it('should render in Nexus 5', async () => {
       browser = await playwright.chromium.launch();
       const nexus5 = playwright.devices['Nexus 5'];
       const context = await browser.newContext({
@@ -45,11 +42,11 @@ describe('Main & Finish Page', () => {
       });
       page = await context.newPage();
       await page.goto('http://localhost:8080/');
-      await page.screenshot({ path: 'test/screenshots/main_page_nexus5.png', fullPage: true });
+      await page.screenshot({ path: 'test/screenshots/main_page--nexus5.png', fullPage: true });
       await page.locator('#submitBtn').click();
       setTimeout(function () {
       }, 1000);
-      await page.screenshot({ path: 'test/screenshots/finish_page_nexus5.png', fullPage: true });
+      await page.screenshot({ path: 'test/screenshots/finish_page--nexus5.png', fullPage: true });
     });
 
     after(async () => {
